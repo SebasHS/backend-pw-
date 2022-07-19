dd7e0bde1bc3b88fa6e5898c9f622d204d765037
Sebas <psvitasebas@gmail.com>
2022-07-19 02:34:44 -0500

    editar perfil

--- frontend/src/pages/editar-perfil/index.jsx
+++ frontend/src/pages/editar-perfil/index.jsx
@@ -1,121 +1,161 @@
-import "./index.css"
-import React from "react"
-import NavigationTo from "../../componets/NavigationTo"
-const Pagina_EP = () => {
-    const [nombrePaciente, setNombrePaciente] = React.useState("")
-    const [apellidosPaciente, setApellidosPaciente] = React.useState("")
-    const [userNamePaciente, setUserNamePaciente] = React.useState("")
-    const [emailPaciente, setEmailPaciente] = React.useState("")
-    const [contraseniaPaciente, setContraseniaPaciente] = React.useState("")
-    const [confircontraseniaPaciente, setConfirContraseniaPaciente] = React.useState("")
-    const [celPaciente, setCelPaciente] = React.useState("")
+import React, { useEffect, useState } from 'react'
+import NavigationTo from '../../componets/NavigationTo'
+import usePerfilService from '../../services/perfil'
+import useAppContext from '../../hooks/useAppContext'
+import { Formik } from 'formik'
+import { useNavigate } from 'react-router-dom'
+import './index.css'
 
+const useProfile = () => {
+    const [perfil, setPerfil] = useState()
+    const { verPerfil, actualizarPerfil } = usePerfilService()
 
-    const onNombrePacienteChange = (evt) => {
-        setNombrePaciente(evt.target.value)
-    }
-    const onApellidosPacienteChange = (evt) => {
-        setApellidosPaciente(evt.target.value)
-    }
-    const onUserNamePacienteChange = (evt) => {
-        setUserNamePaciente(evt.target.value)
-    }
-    const onEmailPacienteChange = (evt) => {
-        setEmailPaciente(evt.target.value)
-    }
-    const onContraseniaChange = (evt) => {
-        setContraseniaPaciente(evt.target.value)
-    }
-    const onConfirContraseniaChange = (evt) => {
-        setConfirContraseniaPaciente(evt.target.value)
-    }
-    const onCelPacienteChange = (evt) => {
-        setCelPaciente(evt.target.value)
+    const obtenerPerfil = async () => {
+        const response = await verPerfil()
+        setPerfil(response.data.data)
     }
 
-
-    const registrarPacienteOnClick = () => {
-        const datosPaciente = {
-            nombre : nombrePaciente,
-            apellidos : apellidosPaciente,
-            UserName : userNamePaciente,
-            Email : emailPaciente,
-            contrasenia : contraseniaPaciente,
-            confircontrasenia : confircontraseniaPaciente,
-            cel: celPaciente
-        }
-        console.log("Se ha guardado el edit del paciente", datosPaciente)
+    const _actualizarPerfil = async (data) => {
+        await actualizarPerfil(data)
     }
-       
+
+    useEffect(() => {
+        obtenerPerfil()
+    }, [])
+
+    return {
+        perfil,
+        obtenerPerfil,
+        actualizarPerfil: _actualizarPerfil
+    }
+}
+
+const Input = ({ label, onChange, value }) => {
     return (
-        <div className="fondo2">
-            <div className="mt-2">
-                <div className="card mb-5">
-                    <div className="card-header encabezadoPerfil">
-                        <h4>
-                            <div>
-                                <NavigationTo href="/inicio-paciente">Principal</NavigationTo>
-                                /
-                                <NavigationTo href="#">EditarPerfil</NavigationTo>
-                            </div>
-                        </h4>
-                        <h1 id="LetraHeader">EDITAR PERFIL</h1>
-                    </div>
-                </div>
-                <div className="container">
-                    <div className="row mt-5">
-                        <div className="col-sm-6" >
-                            <label className="form-label Labels">Nombres</label>
-                            <input type={"text"} className="form-control cuadroLleno" value={nombrePaciente} onChange={onNombrePacienteChange} />
-                        </div>
-                        <div className="col-sm-6">
-                            <label className="form-label Labels">Apellidos</label>
-                            <input type={"text"} className="form-control cuadroLleno" value={apellidosPaciente} onChange={onApellidosPacienteChange} />
-                        </div>
-                    </div>
-
-                    <div className="row mt-4">
-                        <div className="col-sm-6">
-                            <label className="form-label Labels">Usuario</label>
-                            <input type={"text"} className="form-control cuadroLleno" value={userNamePaciente} onChange={onUserNamePacienteChange} />
-                        </div>
-                        <div className="col-sm-6">
-                            <label className="form-label Labels">Correo</label>
-                            <input type={"text"} className="form-control cuadroLleno" value={emailPaciente} onChange={onEmailPacienteChange} />
-                        </div>
-                    </div>
-
-                    <div className="row mt-4">
-                        <div className="col-sm-6">
-                            <label className="form-label Labels">Nueva Contraseña</label>
-                            <input type={"password"} className="form-control cuadroLleno" value={contraseniaPaciente} onChange={onContraseniaChange} />
-                        </div>
-                    </div>
-
-                    <div className="row mt-4">
-                        <div className="col-sm-6">
-                            <label className="form-label Labels">Confirmar Nueva Contraseña</label>
-                            <input type={"password"} className=" cuadroLleno form-control" value={confircontraseniaPaciente} onChange={onConfirContraseniaChange} />
-                        </div>
-                    </div>
-
-                    <div className="row mt-4">
-                        <div className="col-sm-6">
-                            <label className="form-label Labels">Teléfono</label>
-                            <input type={"text"} className="cuadroLleno form-control" value={celPaciente} onChange={onCelPacienteChange} />
-                        </div>
-                    </div>
-
-                    <div className="Boton">
-                        <button id="BotonGuardar" className="mt-5 btn btn--outline-secondary" onClick={registrarPacienteOnClick} type="button">
-                            <NavigationTo id="LetraGuardar" href="/inicio-paciente">Guardar</NavigationTo>
-                        </button>
-                    </div>
-                </div>
-
-            </div>
-        </div>
+        <>
+            <label className="form-label mt-2 h5" id="letra">
+                {label}
+            </label>
+            <input
+                value={value}
+                onChange={onChange}
+                type="text"
+                className="form-control mt-2 mb-2"
+            />
+        </>
     )
 }
 
-export default Pagina_EP
+const DatosDePaciente = () => {
+    const { perfil, actualizarPerfil } = useProfile()
+    const { setLoading } = useAppContext()
+    const navigation = useNavigate()
+
+    const onSubmit = async (values, helper) => {
+        setLoading(true)
+        try {
+            await actualizarPerfil(values)
+            navigation('/')
+        } catch (error) {
+            alert(error.message)
+        }
+        setLoading(false)
+    }
+
+    if (!perfil) {
+        return null
+    }
+
+    return (
+        <Formik initialValues={{ ...perfil }} onSubmit={onSubmit}>
+            {({ handleSubmit, values, handleChange }) => {
+                return (
+                    <form onSubmit={handleSubmit}>
+                        <div className="fondo2">
+                            <div className="mt-2">
+                                <div className="card mb-5">
+                                    <div className="card-header encabezadoPerfil">
+                                        <h4>
+                                            <div>
+                                                <NavigationTo href="/inicio-paciente">Principal</NavigationTo>
+                                                /
+                                                <NavigationTo href="#">EditarPerfil</NavigationTo>
+                                            </div>
+                                        </h4>
+                                        <h1 id="LetraHeader">EDITAR PERFIL</h1>
+                                    </div>
+                                </div>
+                                <div className="container">
+                                    <div className="row mt-5">
+                                        <div className="col-sm-6" >
+                                            <Input
+                                                label={'Nombres '}
+                                                value={values.nombre}
+                                                onChange={handleChange('nombre')}
+                                            />
+                                        </div>
+
+                                        <div className="col-sm-6">
+                                            <Input
+                                                label={'Apellidos '}
+                                                value={values.apellidoPaterno}
+                                                onChange={handleChange('apellidoPaterno')}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className="row mt-4">
+                                        <div className="col-sm-6">
+                                            <label className="form-label Labels">Usuario</label>
+                                            <input type={"text"} className="form-control cuadroLleno"  />
+                                        </div>
+
+                                        <div className="col-sm-6">
+                                            <Input
+                                                label={'Email:'}
+                                                value={values.email}
+                                                onChange={handleChange('email')}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className="row mt-4">
+                                        <div className="col-sm-6">
+                                            <label className="form-label Labels">Nueva Contraseña</label>
+                                            <input type={"password"} className="form-control cuadroLleno"  />
+                                        </div>
+                                    </div>
+
+                                    <div className="row mt-4">
+                                        <div className="col-sm-6">
+                                            <label className="form-label Labels">Confirmar Nueva Contraseña</label>
+                                            <input type={"password"} className=" cuadroLleno form-control" />
+                                        </div>
+                                    </div>
+
+                                    <div className="row mt-4">
+                                        <div className="col-sm-6">
+                                            <Input
+                                                label={'Telefono:'}
+                                                value={values.numContacto}
+                                                onChange={handleChange('numContacto')}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className="d-grid gap-2 d-md-flex justify-content-md-end mt-2 Boton">
+                                        <input className="btn btn-primary" type={'submit'}>
+                
+                                        </input>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                )
+            }}
+        </Formik>
+    )
+}
+
+export default DatosDePaciente
